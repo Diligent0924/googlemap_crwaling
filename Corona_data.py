72727037f49df989b36a1fdfe7268d6d9b8f7420
Diligent0924 <62194486+Diligent0924@users.noreply.github.com>
2021-03-14 17:43:39 +0900

    Add files via upload

---
+++ Corona_data.py
@@ -0,0 +1,34 @@
+import Def.googlemap
+
+my_url = soup_obj.select("em.info_variation")
+cnt = 0
+
+key = ["코로나 확진자","검사 중인 환자"]
+value = []
+
+for j in my_url:  # class -> em.info_variation를 순서대로 Parsing한것
+    cnt += 1
+    value.append(j.text)
+    if cnt > 1:
+        break
+
+Corona = {}
+for i in range(len(key)):
+    Corona[key[i]] = value[i]
+    
+# 해당 날짜
+now = datetime.datetime.now()
+nowdate = now.strftime('%Y-%m-%d')
+
+def getDay():
+    now = time.localtime()
+    of = ['월', '화', '수', '목', '금', '토', '일']
+    return of[now.tm_wday]
+weekday = getDay()
+
+# 프린트 내용
+print(nowdate +  ".("+ weekday+ ")")
+for i in range(0,2):
+    print(key[i] + " : " +  value[i])
+    
+# time.sleep(6000)
